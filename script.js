11bacb7ebe21d0bd87b0819cc581d69aad0955b5
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-06 21:31:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById('change_btn').addEventListener('click',()=>{
+	const blockId=document.getElementById('input-id').value;
+	const colorId=document.getElementById('color-id').value;
+	document.getElementById(blockId).style.backgroundColor=colorId;
+});
+
+document.getElementById('reset').addEventListener('click',()=>{
+	const allBlocks=document.getElementsByClassName('grid-item');
+	for(var i = 0; i < allBlocks.length; i++) {
+        allBlocks[i].style.backgroundColor = 'transparent';
+    }
+});
